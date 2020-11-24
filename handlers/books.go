ad432dcb67736dd7973a7885443e13003c1a05fa
hdussert <hdussert@student.42.fr>
2020-11-24 10:12:18 +0100

    Get books

--- handlers/books.go
+++ handlers/books.go
@@ -46,5 +46,20 @@ func PostBook(w http.ResponseWriter, r *http.Request, params map[string]string) 
 }
 
 func GetBooks(w http.ResponseWriter, r *http.Request, params map[string]string) {
+	log.Printf("%+v", *r)
+	query := "SELECT * FROM books"
+	
+	data := []byte{}
 
+	db := DB.OpenConnection()
+	log.Println("JELLOW")
+	err := db.QueryRow(`SELECT COALESCE (array_to_json(array_agg(row_to_json(res))), '[]') FROM (` + query + `) AS res;`).Scan(&data)
+	if err != nil {
+		panic(err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(data)
+
+	defer db.Close()
 }
